2ce5444e74bd3f01724cecdeeb41a33856c594c8
WG <wgrandoms@gmail.com>
2021-12-27 13:20:51 -0500

    Enhanced the form text field using CKEditor to allow rich text inputs

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SubmitField
+from wtforms import SubmitField
 from wtforms.validators import DataRequired, Length
 from flask_sqlalchemy import SQLAlchemy
+from flask_ckeditor import CKEditor, CKEditorField
 
 
 app = Flask(__name__)
@@ -10,6 +11,7 @@ app.config['SECRET_KEY'] = 'foobar'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+ckeditor = CKEditor(app)
 
 
 # create the database and needed models
@@ -22,7 +24,7 @@ db.create_all()
 
 # create needed forms
 class NoteForm(FlaskForm):
-    note = TextAreaField('Notes', 
+    note = CKEditorField('Notes', 
                          validators=[DataRequired(), Length(min=0, max=200)])
     submit = SubmitField('Submit')
 
